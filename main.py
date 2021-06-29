10b410a651b8a712105d96c494a032694be2e7ae
Alex Johnson <ajohnson2389@gmail.com>
2021-06-29 15:53:14 -0400

    syntax modifications

--- main.py
+++ main.py
@@ -31,11 +31,11 @@ print(data.to_dict())
 
 #TODO 1. Create a dictionary in this format:
 # {new_key:new_value for (index, row) in df.iterrows()}
-phonetic_dict = {row.letter:row.code for (index, row) in data.iterrows()}
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 # print(phonetic_dict)
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 word = input("Enter a word: ").upper()
 output_list = [phonetic_dict[letter] for letter in word]
-print (output_list)
+print(output_list)
 
